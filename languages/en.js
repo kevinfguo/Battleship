aaa41b1c69db29ceee686cb713271ecea593b457
kevinfguo <blindspotx2@gmail.com>
2015-12-03 10:20:50 -0500

    Help, languages
    French

--- languages/en.js
+++ languages/en.js
@@ -1,6 +1,7 @@
 window.angularTranslationsLoaded('en',{
-"RULES_OF_TICTACTOE":"Rules of TicTacToe",
-"RULES_SLIDE1":"You and your opponent take turns to mark the grid in an empty spot. The first mark is X, then O, then X, then O, etc.",
-"RULES_SLIDE2":"The first to mark a whole row, column or diagonal wins.",
+"RULES_OF_TICTACTOE":"Rules of Battleship",
+"RULES_SLIDE1": "First, you and your opponent place your 5 battleships on your own board.",
+"RULES_SLIDE2": "You and your opponent then take turns to fire upon the enemy board. If you hit an enemy battleship, you will score a X. If you miss, you will score a O.",
+"RULES_SLIDE3": "The first player to eliminate all opposing battleships is declared the winner."
 "CLOSE":"Close"
 });
---
+++ languages/fr.js
@@ -0,0 +1,7 @@
+window.angularTranslationsLoaded('fr',{
+"RULES_OF_TICTACTOE":"Rules of Battleship",
+"RULES_SLIDE1": "Premier, vous placez 5 navires sur la carte et votre adversaire va faire la meme chose.",
+"RULES_SLIDE2": "A tour de role, attaquer l'adversaire. Si vous réussissez, alors un X indique un succes, autrement un O.",
+"RULES_SLIDE3": "Le premier joueur à éliminer tous les navires est le gagnant."
+"CLOSE":"Close"
+});
